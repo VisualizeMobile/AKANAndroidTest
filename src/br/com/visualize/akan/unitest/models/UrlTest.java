01a71c72e50a76f64a378d63d987d31525e93969
Thaiane <thaiane.unb@hotmail.com>
2015-05-15 01:44:04 -0300

    Implementing unit tests to model Url
    Signed-off-by: Thaiane <thaiane.unb@hotmail.com>
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

---
+++ src/br/com/visualize/akan/unitest/models/UrlTest.java
@@ -0,0 +1,60 @@
+package br.com.visualize.akan.unitest.models;
+
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+import br.com.visualize.akan.domain.model.Url;
+
+
+public class UrlTest extends AndroidTestCase {
+	private Url validUrl = null;
+	private Url nullUrl = null;
+	
+	public void setUp() {
+		this.validUrl = new Url();
+	}
+	
+	public void testValidInstantiationUrl() {
+		Assert.assertNotNull( validUrl );
+	}
+	
+	public void testInvalidInstantiationUrl() {
+		Assert.assertNull( nullUrl );
+	}
+	
+	public void testIdUpdateUrl() {
+		this.validUrl.setIdUpdateUrl( 1 );
+		
+		int result = this.validUrl.getIdUpdateUrl();
+		int expectedResult = 1;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testUpdateVerifierUrl() {
+		this.validUrl.setUpdateVerifierUrl( 1 );
+		
+		int result = this.validUrl.getUpdateVerifierUrl();
+		int expectedResult = 1;
+				
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testDefaultUrl() {
+		this.validUrl.setDefaultUrl( "url test" );
+		
+		String result = this.validUrl.getDefaultUrl();
+		String expectedResult = "url test";
+				
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testFirstAlternativeUrl() {
+		this.validUrl.setFirstAlternativeUrl( "url test" );
+		
+		String result = this.validUrl.getFirstAlternativeUrl();
+		String expectedResult = "url test";
+				
+		Assert.assertEquals( expectedResult, result );
+	}
+}
