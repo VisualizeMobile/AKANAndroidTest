7187ea96ee2ee6f19978cd29f7116cf48b2c73e2
TomazMartins <tomaz.unb@outlook.com>
2015-05-15 00:25:16 -0300

    Implementing unit tests to model Congressman
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- src/br/com/visualize/akan/unitest/models/CongressmanTest.java
+++ src/br/com/visualize/akan/unitest/models/CongressmanTest.java
@@ -21,4 +21,44 @@ public class CongressmanTest extends AndroidTestCase {
 	public void testInvalidInstantiationCongressman() {
 		Assert.assertNull( nullCongressman );
 	}
+	
+	public void testIdCongressman() {
+		this.validCongressman.setIdCongressman( 1 );
+		
+		int result = this.validCongressman.getIdCongressman();
+		int expectedResult = 1;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testIdUpdateCongressman() {
+		this.validCongressman.setIdUpdateCongressman( 1 );
+		
+		int result = this.validCongressman.getIdUpdateCongressman();
+		int expectedResult = 1;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testIsFollowedCongressman() {
+		final boolean FOLLOWED = true;
+		
+		this.validCongressman.setStatusCogressman( FOLLOWED );
+		
+		boolean result = this.validCongressman.isStatusCogressman();
+		boolean expectedResult = FOLLOWED;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testIsNotFollowedCongressman() {
+		final boolean NOT_FOLLOWED = false;
+		
+		this.validCongressman.setStatusCogressman( NOT_FOLLOWED );
+		
+		boolean result = this.validCongressman.isStatusCogressman();
+		boolean expectedResult = NOT_FOLLOWED;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
 }
