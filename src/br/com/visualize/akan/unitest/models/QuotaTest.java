973647e7d5cc32ac0509bde625128d39876d5e90
Thaiane <thaiane.unb@hotmail.com>
2015-05-15 01:21:11 -0300

    Implementing unit tests to model Quota
    Signed-off-by: Thaiane <thaiane.unb@hotmail.com>
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- src/br/com/visualize/akan/unitest/models/QuotaTest.java
+++ src/br/com/visualize/akan/unitest/models/QuotaTest.java
@@ -4,6 +4,8 @@ package br.com.visualize.akan.unitest.models;
 import junit.framework.Assert;
 import android.test.AndroidTestCase;
 import br.com.visualize.akan.domain.model.Quota;
+import br.com.visualize.akan.domain.enumeration.Month;
+import br.com.visualize.akan.domain.enumeration.SubQuota;
 
 
 public class QuotaTest extends AndroidTestCase {
@@ -36,6 +38,33 @@ public class QuotaTest extends AndroidTestCase {
 		
 		int result = this.validQuota.getIdUpdateQuota();
 		int expectedResult = 1;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testTypeQuota() {
+		this.validQuota.setTypeQuotaByNumber( 0 );
+		
+		SubQuota result = this.validQuota.getTypeQuota();
+		SubQuota expectedResult = SubQuota.WITHOUT_TYPE;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testMonthReferenceQuota() {
+		this.validQuota.setTypeMonthByNumber( 1 );
+		
+		Month result = this.validQuota.getMonthReferenceQuota();
+		Month expectedResult = Month.JANUARY;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testYearQuota() {
+		this.validQuota.setYearReferenceQuota( 2010 );
+		
+		int result = this.validQuota.getYearReferenceQuota();
+		int expectedResult = 2010;
 		
 		Assert.assertEquals( expectedResult, result );
 	}
