53ca1689fbe5e4430ebee23c6ae86c18dcd10011
TomazMartins <tomaz.unb@outlook.com>
2015-05-16 18:56:04 -0300

    Implementing unit test to model Statistic
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- src/br/com/visualize/akan/unitest/models/StatisticTest.java
+++ src/br/com/visualize/akan/unitest/models/StatisticTest.java
@@ -3,6 +3,7 @@ package br.com.visualize.akan.unitest.models;
 
 import junit.framework.Assert;
 import android.test.AndroidTestCase;
+import br.com.visualize.akan.domain.enumeration.Month;
 import br.com.visualize.akan.domain.enumeration.SubQuota;
 import br.com.visualize.akan.domain.model.Statistic;
 
@@ -10,11 +11,14 @@ import br.com.visualize.akan.domain.model.Statistic;
 public class StatisticTest extends AndroidTestCase {
 	private Statistic validStatistic = null;
 	private Statistic nullStatistic = null;
+	
 	private SubQuota validSubQuota = null;
+	private Month validMonth = null;
 	
 	public void setUp() {
 		this.validStatistic = new Statistic();
 		this.validSubQuota = SubQuota.WITHOUT_TYPE;
+		this.validMonth = Month.WITHOUT_MONTH;
 	}
 	
 	public void testValidInstantiationStatistic() {
@@ -22,7 +26,7 @@ public class StatisticTest extends AndroidTestCase {
 	}
 	
 	public void testInvalidInstantiationStatistic() {
-		Assert.assertNotNull( nullStatistic );
+		Assert.assertNull( nullStatistic );
 	}
 	
 	public void testSubQuota() {
@@ -30,6 +34,33 @@ public class StatisticTest extends AndroidTestCase {
 		
 		SubQuota result = this.validStatistic.getSubquota();
 		SubQuota expectedResult = SubQuota.WITHOUT_TYPE;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testYear() {
+		this.validStatistic.setYear( 2010 );
+		
+		int result = this.validStatistic.getYear();
+		int expectedResult = 2010;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testMonth() {
+		this.validStatistic.setMonth( validMonth );
+		
+		Month result = this.validStatistic.getMonth();
+		Month expectedResult = Month.WITHOUT_MONTH;
+		
+		Assert.assertEquals( expectedResult, result );
+	}
+	
+	public void testStdDeviation() {
+		this.validStatistic.setStdDeviation( 15.887 );
+		
+		double result = this.validStatistic.getStdDeviation();
+		double expectedResult = 15.887;
 		
 		Assert.assertEquals( expectedResult, result );
 	}
